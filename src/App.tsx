e506c677e8bb201876be83b786ef9ce4cadbd1d1
sanjar@home <fr.niez@gmail.com>
2021-12-15 11:33:50 +0600

    wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import './App.css';
-// import LineChart from "./app/components/lineChart/LineChart";
-// import { BarChart } from './app/components/barChart/BarChart';
+import Home from "./app/pages/home/Home";
 import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      {/*<LineChart/>*/}
-      {/*<BarChart/>*/}
+
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
       </Routes>
     </div>
   );
---
+++ src/app/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import LineChart from "../../components/lineChart/LineChart";
+import {BarChart} from "../../components/barChart/BarChart";
+
+function Home() {
+  return (
+      <div>
+        <LineChart/>
+        <BarChart/>
+      </div>
+  );
+}
+
+export default Home;
