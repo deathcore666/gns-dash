a421c74321d80ee91dc3154edaa30d6f7a1a89f9
beknazar <beknazaraibekuulu@mail.ru>
2021-12-16 10:48:48 +0600

    sign

--- src/profile/sign-in.tsx
+++ src/profile/sign-in.tsx
@@ -7,24 +7,18 @@ export const SignIn = () => {
   return (
     <div>
       <div className={styles.borderR}>
+
         <Grid
           container
           direction="row"
           justifyContent="center"
           alignItems="center"
         >
-        </Grid>
-        <Grid
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-        >
-          <Grid item xs={6} md={4}>
+          <Grid item xs={6} md={3}>
             <SignInForm />
           </Grid>
           <Grid item xs={6} md={6}>
-          <div className={styles.txtSpan}>
+                            <div className={styles.txtSpan}>
                 <div>
                   <span>ККМ:</span>20438
                 </div>
@@ -39,7 +33,7 @@ export const SignIn = () => {
                   <span>Проверка субьектов:</span>20438
                 </div>
               </div>
-            <img alt="kg" src="/img/kg.svg" width={990} height={465} />
+            <img alt="kg" src="/img/kg.svg" width={1000} height={465} />
           </Grid>
         </Grid>
       </div>
