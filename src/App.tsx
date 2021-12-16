716314bc96b38dac9ec32cf776e1f4ea9331cd99
beknazar <beknazaraibekuulu@mail.ru>
2021-12-16 16:46:46 +0600

    added menu component

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react';
 import './App.css';
 import Home from "./app/pages/home/Home";
 import { Routes, Route } from "react-router-dom";
+import MainMenu from './app/pages/mian-menu/mainMenu';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<MainMenu />} />
       </Routes>
     </div>
   );
