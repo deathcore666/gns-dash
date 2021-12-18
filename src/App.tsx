58b9b211e203f9f17fc73002e1946a30fccb0270
sanjar@home <fr.niez@gmail.com>
2021-12-18 18:39:17 +0600

    app.ts tabulation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.css';
 import React from 'react';
 import './App.css';
-import Home from "./app/pages/home/Home";
-import { Routes, Route } from "react-router-dom";
-import Itas from "./app/pages/itas/Itas";
+import { Routes, Route } from 'react-router-dom';
+import Itas from './app/pages/itas/Itas';
 import MainMenu from './app/pages/mian-menu/mainMenu';
-import CardInfo from './app/pages/card/cardInfo';
 
 function App() {
   return (
-    <div className="App">
-      <Routes>
-        <Route path="/" element={<MainMenu />} />
-        <Route path="/itas" element={<Itas />} />
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={ <MainMenu/> }/>
+          <Route path="/itas" element={ <Itas/> }/>
+          <Route path="/itas" element={ <Itas/> }/>
+        </Routes>
+      </div>
   );
 }
 
