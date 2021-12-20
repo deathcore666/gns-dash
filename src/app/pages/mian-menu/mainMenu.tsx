d1e94bdeb46c6292dcdce7786df8d5e34c02ed6d
beknazar <beknazaraibekuulu@mail.ru>
2021-12-20 17:10:24 +0600

    сделал через грид

--- src/app/pages/mian-menu/mainMenu.tsx
+++ src/app/pages/mian-menu/mainMenu.tsx
@@ -12,67 +12,87 @@ import mtLogo from '../../../assets/img/logos/MT.svg'
 function MainMenu() {
   return (
     <Container maxWidth="lg">
-      <Grid container  >
-        <Menu
-          title="Иснак"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={isnakLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="Кабинет НП"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={cabinetLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="ACB"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={acbLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="ЭСФ"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={esfLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="Е-патент"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={patentLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="Свободный пост"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={postLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="ККМ"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={kkmLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="ЭТТН"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={cabinetLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="Дислокация"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={cabinetLogo}
-          url="https://salyk.kg/"
-        />
-        <Menu
-          title="ГАИС МТ"
-          value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
-          img={mtLogo}
-          url="https://salyk.kg/"
-        />
+      <Grid container direction="row">
+        <Grid item xs={3}>
+          <Menu
+            title="Иснак"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={isnakLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="Кабинет НП"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={cabinetLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="ACB"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={acbLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="ЭСФ"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={esfLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="Е-патент"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={patentLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="Свободный пост"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={postLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="ККМ"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={kkmLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="ЭТТН"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={cabinetLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="Дислокация"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={cabinetLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
+        <Grid item xs={3}>
+          <Menu
+            title="ГАИС МТ"
+            value="Lorem ipsum dolor sit amet consectetur adipisicing elit HELLO"
+            img={mtLogo}
+            url="https://salyk.kg/"
+          />
+        </Grid>
       </Grid>
     </Container>
   );
